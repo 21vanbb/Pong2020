6fe86c221e7334eeec6d4f5c6e492a2aeedea6bf
Brenna Vanbuskirk <21VanbB@live.elida.k12.oh.us>
2020-11-19 09:12:50 -0500

    Fifth Commit
    Working on paddle movement script

--- Pong2020/Assets/Scripts/paddleMovement.cs
+++ Pong2020/Assets/Scripts/paddleMovement.cs
@@ -7,7 +7,7 @@ public class paddleMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+       public float xRange = 
     }
 
     // Update is called once per frame
