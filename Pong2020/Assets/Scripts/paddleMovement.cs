2ecd7de100ffdb3c1bb93490953310b1b7f9aa8a
Brenna Vanbuskirk <21VanbB@live.elida.k12.oh.us>
2020-11-20 09:12:54 -0500

    Paddle Movement
    The paddle goes left and right with the arrow keys

--- Pong2020/Assets/Scripts/paddleMovement.cs
+++ Pong2020/Assets/Scripts/paddleMovement.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class paddleMovement : MonoBehaviour
 {
+    public float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
-       public float xRange = 
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
     }
 }
