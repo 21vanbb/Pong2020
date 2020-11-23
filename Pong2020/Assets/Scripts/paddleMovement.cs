5cc9aec8cef9183a584d3609340ea3afa8a77732
Brenna Vanbuskirk <21VanbB@live.elida.k12.oh.us>
2020-11-23 08:42:19 -0500

    Paddle Movement
    it stays in range of the screen

--- Pong2020/Assets/Scripts/paddleMovement.cs
+++ Pong2020/Assets/Scripts/paddleMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class paddleMovement : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float xRange = 6.9f; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +23,17 @@ public class paddleMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+    }
+    private void LateUpdate()
+    {
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector2(xRange, transform.position.y);
+        }
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector2(-xRange, transform.position.y);
         }
     }
 }
