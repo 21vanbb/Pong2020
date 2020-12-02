7a05f2962f415e411c3d797a95e66ebf6cb29ac4
Brenna Vanbuskirk <21VanbB@live.elida.k12.oh.us>
2020-12-02 09:15:30 -0500

    got rid of ball movement
    found a tutorial to watch on youtube.

--- Pong2020/Assets/Scripts/ballMovement.cs
+++ Pong2020/Assets/Scripts/ballMovement.cs
@@ -7,29 +7,11 @@ public class ballMovement : MonoBehaviour
     public float yRange = 4.8f;
     public float xRange = 6.9f;
     public float MoveSpeed;
-    public float height = 0.5f;
-    private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        rb.velocity = RandomVector(-5f, 5f);
-        transform.gameObject.GetOrAddComponent<ColliderEventSystem>().ColliderEntered += PushObject;
-    }
-
-    private void PushObject(ColliderEventSystem eventTarget, Collider2D other)
-    {
-        float speed = 100;
-        Vector3 direction = (transform.position - other.transform.position).normalized;
-        rb.Addforce((direction * speed));
-    }
-
-    private Vector3 RandomVector(float min, float max)
-    {
-        var x = UnityEngine.Random.Range(min, max);
-        var y = UnityEngine.Random.Range(min, max);
-        return new Vector3(x, y);
+        
     }
 
     // Update is called once per frame
