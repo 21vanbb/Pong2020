bd297f83d9f02d68b5fd7681d8ca257f44cb6828
Brenna Vanbuskirk <21VanbB@live.elida.k12.oh.us>
2020-12-08 09:04:41 -0500

    paddle script
    now it's on the screen but it doesn't move.

--- Pong2020/Assets/Scripts/paddleScript.cs
+++ Pong2020/Assets/Scripts/paddleScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class paddleScript : MonoBehaviour
 {
     public float speed;
+    public float rightEdge;
+    public float leftEdge;
 
     // Start is called before the first frame update
     void Start()
@@ -17,5 +19,13 @@ public class paddleScript : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         transform.Translate (Vector2.right * horizontal * Time.deltaTime * speed);
+        if(transform.position.x < leftEdge)
+        {
+            transform.position = new Vector2(leftEdge, transform.position.y);
+        }
+        if (transform.position.x > rightEdge)
+        {
+            transform.position = new Vector2(rightEdge, transform.position.y);
+        }
     }
 }
