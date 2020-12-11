ec0cd9db937853844c0dabfe4815ee41f642d66b
Brenna Vanbuskirk <21VanbB@live.elida.k12.oh.us>
2020-12-11 09:15:27 -0500

    even more paddle movement
    trying a new tutorial

--- Pong2020/Assets/Scripts/paddleScript.cs
+++ Pong2020/Assets/Scripts/paddleScript.cs
@@ -4,44 +4,18 @@ using UnityEngine;
 
 public class paddleScript : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float xRange = 6.9f;
-    public float leftEdge;
+    public float speed;
+    private RigidBody2D rb; 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetCompnonent<RigidBody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        transform.Translate (Vector2.right * horizontal * Time.deltaTime * speed);
-        if(transform.position.x < leftEdge)
-        {
-            transform.position = new Vector2(leftEdge, transform.position.y);
-        }
-        if (transform.position.x > xRange)
-        {
-            transform.position = new Vector2(xRange, transform.position.y);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            Vector3 position = this.transform.position;
-            position.x--;
-            this.transform.position = position;
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            Vector3 position = this.transform.position;
-            position.x++;
-            this.transform.position = position;
-        }
-        {
-            var move = new Vector3(Input.GetAxis("Horizontal"), 0);
-            transform.position += move * speed * Time.deltaTime;
-        }
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal") );
     }
 }
